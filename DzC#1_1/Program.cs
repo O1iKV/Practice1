5aa72a77896e9b12c8bb65105ec926422ea1becb
KarpovaOV <olikarpovabusuu@yandex.ru>
2023-08-14 23:32:26 +0400

    1_1

--- DzC#1_1/Program.cs
+++ DzC#1_1/Program.cs
@@ -3,3 +3,22 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+﻿Console.WriteLine("Пожалуйста, введите 2 числа по очереди для сравнения.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число " + num1 + " большее");
+    Console.Write("Число " + num2 + " меньшее");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Число " + num1 + " равно числу " + num2);
+}
+else
+{
+    Console.WriteLine("Число " + num2 + " большее");
+    Console.Write("Число " + num1 + " меньшее");
+}
